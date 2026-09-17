e60fe7a418f240ac89202a061b91de661ad3e9f6
Yuliia Tkachenko <870057704@edu.taitotalo.fi>
2026-09-17 14:40:06 +0300

    shop (fourth version)

--- shop.py
+++ shop.py
@@ -4,7 +4,7 @@ def open_shop():
     print("2. Buy an apple")
     print("3. Buy a diamond")
     print("4. Buy a dog")
-
+    print("5. I don't want to buy anything")
 
     choice = input("Choose and write a number(1-4): ")
     if choice == "1":
@@ -18,6 +18,9 @@ def open_shop():
     
     elif choice == "4":
         print("You bought a dog!")
+
+    elif choice == "5":
+        print("You don't buy anything!")
   
     else: 
         print ("Wrong choice!")
